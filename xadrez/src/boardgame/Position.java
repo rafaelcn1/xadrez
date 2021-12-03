10e54b0559b1ed3050b66bfb1d24697435d029af
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-03 15:30:26 -0300

    Criação da classe Position

---
+++ xadrez/src/boardgame/Position.java
@@ -0,0 +1,40 @@
+package boardgame;
+
+public class Position {
+
+	// Classe posição no pacote boardgame correspondente de tabuleiro
+
+	private int row; // linha
+	private int column; // coluna
+
+	public void setValues(int row, int column) {
+
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+
+}
