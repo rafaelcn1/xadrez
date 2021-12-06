4d4d9677a54aa93ba6fe4c833e58f1a108f5907a
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-06 11:42:26 -0300

    Criação da peça King (Rei)

--- xadrez/src/chess/pieces/King.java
+++ xadrez/src/chess/pieces/King.java
@@ -4,17 +4,18 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class King extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public King(Board board, Color color) {
 		super(board, color);
+		// TODO Auto-generated constructor stub
 	}
 
 	// Metodo ToString retorna apenas a letra, pq a letra irá ser impressa no
 	// tabuleiro
 	@Override
 	public String toString() {
-		return "R";
+		return "K";
 	}
 
 }
