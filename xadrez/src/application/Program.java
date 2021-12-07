1220dee67d69f692205efc7f4bea19386a7c555c
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-07 15:51:50 -0300

    Testando o makeMovie

--- xadrez/src/application/Program.java
+++ xadrez/src/application/Program.java
@@ -1,14 +1,34 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
-		
+
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("DIGITE A POSIÇÃO DE ORIGEM DA PEÇA: ");
+			ChessPosition source = UI.readChessPostion(sc);
+			
+			System.out.println();
+			
+			System.out.print("DIGITE A POSIÇÃO DE DESTINO DA PEÇA: ");
+			ChessPosition target = UI.readChessPostion(sc);
+			
+			ChessPiece capturePiece = chessMatch.performChessMove(source, target);
+			
+
+		}
+
 	}
 
 }
