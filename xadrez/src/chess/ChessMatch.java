b0e2e4877456a0e8294341edb9c634ae03a9f348
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-14 14:35:26 -0300

    Criando o metodo King Color

--- xadrez/src/chess/ChessMatch.java
+++ xadrez/src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import boardgame.Board;
 import boardgame.Piece;
@@ -94,9 +95,20 @@ public class ChessMatch {
 			throw new ChessException("A peca escolhida nao pode se mover para a posicao de destino!");
 		}
 	}
-	
+
 	private Color opponent(Color color) {
 		return (color == color.BRANCA ? color.PRETA : color.BRANCA);
+	}
+
+	private ChessPiece king(Color color) {
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == color)
+				.collect(Collectors.toList());
+		for (Piece p : list) {
+			if (p instanceof King) {
+				return (ChessPiece) p;
+			}
+		}
+		throw new IllegalStateException("Nao existe um Rei da cor " + color + " no tabuleiro!");
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
