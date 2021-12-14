48c2d971ae07675fe37e638e143d65f456ed414c
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-14 14:18:58 -0300

    Desfazendo movimento !

--- xadrez/src/chess/ChessMatch.java
+++ xadrez/src/chess/ChessMatch.java
@@ -64,6 +64,17 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 
+	public void undoMove(Position source, Position target, Piece capturedPiece) {
+		Piece p = board.removePiece(target);
+		board.placePiece(p, source);
+
+		if (capturedPiece != null) {
+			board.placePiece(capturedPiece, target);
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+		}
+	}
+
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao existe peca na posicao de origem!");
