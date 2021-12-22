dbb69407670ca3faaa2dcb825108107274e1cf4c
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2021-12-22 09:43:08 -0300

    Jogo finalizado!

--- xadrez/src/chess/ChessMatch.java
+++ xadrez/src/chess/ChessMatch.java
@@ -334,39 +334,41 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('e', 8, new King(board, Color.PRETA, this));
+
 		placeNewPiece('e', 1, new King(board, Color.BRANCA, this));
+		placeNewPiece('d', 1, new Queen(board, Color.BRANCA));
+		placeNewPiece('a', 1, new Rook(board, Color.BRANCA));
+		placeNewPiece('c', 1, new Bishop(board, Color.BRANCA));
+		placeNewPiece('f', 1, new Bishop(board, Color.BRANCA));
+		placeNewPiece('b', 1, new Knight(board, Color.BRANCA));
+		placeNewPiece('g', 1, new Knight(board, Color.BRANCA));
+		placeNewPiece('h', 1, new Rook(board, Color.BRANCA));
+		placeNewPiece('a', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('b', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('c', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('d', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('e', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('f', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('g', 2, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('h', 2, new Pawn(board, Color.BRANCA, this));
 
-		placeNewPiece('a', 7, new Pawn(board, Color.BRANCA, this));
+		placeNewPiece('e', 8, new King(board, Color.PRETA, this));
+		placeNewPiece('b', 8, new Knight(board, Color.PRETA));
+		placeNewPiece('g', 8, new Knight(board, Color.PRETA));
+		placeNewPiece('c', 8, new Bishop(board, Color.PRETA));
+		placeNewPiece('f', 8, new Bishop(board, Color.PRETA));
+		placeNewPiece('a', 8, new Rook(board, Color.PRETA));
+		placeNewPiece('h', 8, new Rook(board, Color.PRETA));
+		placeNewPiece('d', 8, new Queen(board, Color.PRETA));
+		placeNewPiece('a', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('b', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('c', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('d', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('e', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('f', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('g', 7, new Pawn(board, Color.PRETA, this));
+		placeNewPiece('h', 7, new Pawn(board, Color.PRETA, this));
 
-		/*
-		 * placeNewPiece('d', 1, new Queen(board, Color.BRANCA)); placeNewPiece('a', 1,
-		 * new Rook(board, Color.BRANCA)); placeNewPiece('c', 1, new Bishop(board,
-		 * Color.BRANCA)); placeNewPiece('f', 1, new Bishop(board, Color.BRANCA));
-		 * placeNewPiece('b', 1, new Knight(board, Color.BRANCA)); placeNewPiece('g', 1,
-		 * new Knight(board, Color.BRANCA)); placeNewPiece('h', 1, new Rook(board,
-		 * Color.BRANCA)); placeNewPiece('a', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('b', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('c', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('d', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('e', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('f', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('g', 2, new Pawn(board, Color.BRANCA, this));
-		 * placeNewPiece('h', 2, new Pawn(board, Color.BRANCA, this));
-		 * 
-		 * placeNewPiece('b', 8, new Knight(board, Color.PRETA)); placeNewPiece('g', 8,
-		 * new Knight(board, Color.PRETA)); placeNewPiece('c', 8, new Bishop(board,
-		 * Color.PRETA)); placeNewPiece('f', 8, new Bishop(board, Color.PRETA));
-		 * placeNewPiece('a', 8, new Rook(board, Color.PRETA)); placeNewPiece('h', 8,
-		 * new Rook(board, Color.PRETA)); placeNewPiece('d', 8, new Queen(board,
-		 * Color.PRETA)); placeNewPiece('a', 7, new Pawn(board, Color.PRETA, this));
-		 * placeNewPiece('b', 7, new Pawn(board, Color.PRETA, this)); placeNewPiece('c',
-		 * 7, new Pawn(board, Color.PRETA, this)); placeNewPiece('d', 7, new Pawn(board,
-		 * Color.PRETA, this)); placeNewPiece('e', 7, new Pawn(board, Color.PRETA,
-		 * this)); placeNewPiece('f', 7, new Pawn(board, Color.PRETA, this));
-		 * placeNewPiece('g', 7, new Pawn(board, Color.PRETA, this)); placeNewPiece('h',
-		 * 7, new Pawn(board, Color.PRETA, this));
-		 */
 	}
 
 	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
